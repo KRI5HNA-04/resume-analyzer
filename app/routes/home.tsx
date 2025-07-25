4ab07f587a01e1dbd4d57cdb44abf133e3081f05
Krishna <king181219@gmail.com>
2025-07-25 21:20:41 +0530

    home page updates

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,40 @@
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeAI" },
+    {
+      name: "description",
+      content: "Welcome to ResumeAI! Your placement companion",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Application & Ratings</h1>
+          <h2>
+            Upload your resume and get a detailed analysis of your application
+            status.
+          </h2>
+        </div>
+      </section>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
